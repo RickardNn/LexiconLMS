79df82f447b5914be606e98f108903ede5d8cf5e
Rickard <rickard.nn@gmail.com>
2016-02-24 12:00:05 +0100

    Initial commit

---
+++ Lexicon_LMS/Migrations/201602231545046_init.cs
@@ -0,0 +1,22 @@
+namespace Lexicon_LMS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "GroupId", c => c.Int(nullable: false));
+            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "LastName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "LastName");
+            DropColumn("dbo.AspNetUsers", "FirstName");
+            DropColumn("dbo.AspNetUsers", "GroupId");
+        }
+    }
+}
---
+++ Lexicon_LMS/Models/Document.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lexicon_LMS.Models
+{
+    public class Document
+    {
+        public int DocumentId { get; set; }
+        public int ApplicationUserId { get; set; }
+        public int? GroupId { get; set; }
+        public int? CourseId { get; set; }
+        public int? ActivityId { get; set; }
+        public string Title { get; set; }
+        public string FileName { get; set; }
+        public string Description { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+        public virtual Group Group { get; set; }
+        public virtual Course Course { get; set; }
+        public virtual Activity Activity { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
