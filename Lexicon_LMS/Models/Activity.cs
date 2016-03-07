abda1bb171b177de3c0e32f1bf57fa85e6f2d9fb
Disa Hiltunen <disa@spruce.se>
2016-03-07 09:42:47 +0100

    Ändrat i activity controller så man ser aktiviteter för rätt kurs. Uppdatterad navbar.

--- Lexicon_LMS/Models/Activity.cs
+++ Lexicon_LMS/Models/Activity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +10,28 @@ namespace Lexicon_LMS.Models
     public class Activity
     {
         public int ActivityId { get; set; }
+
+     
         public int CourseId { get; set; }
+
+        [DisplayName("Typ")]
         public string Type { get; set; }
+
+        [DisplayName("Aktivitet")]
         public string Name { get; set; }
+
+        [DisplayName("Lärare")]
         public string Teacher { get; set; }
+
+        [DisplayName("Beskrivning")]
         public string Description { get; set; }
+        
+        [DisplayName("Startdatum")]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
+
+        [DisplayName("Slutdatum")]
+        [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
 
         public virtual Course Course { get; set; }
